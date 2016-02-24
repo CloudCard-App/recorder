3bd87db9e4336e60ba015790a25211f0f3d4ac34
Marc Bacvanski <marc.bacvanski@gmail.com>
2016-02-24 10:37:05 -0800

    Reinstalled error handlers
    They were commented out for testing, now they're back in.

--- app.js
+++ app.js
@@ -28,36 +28,36 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
 // Should be after all routing is done
-//app.use(function (req, res, next) {
-//    console.log("Not found!");
-//    var err = new Error('Not Found');
-//    err.status = 404;
-//    next(err);
-//});
+app.use(function (req, res, next) {
+    console.log("Not found!");
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
-//error handlers
+// error handlers
 
-//development error handler
-//will print stacktrace
-//if (app.get('env') === 'development') {
-//    app.use(function (err, req, res, next) {
-//        res.status(err.status || 500);
-//        res.render('error', {
-//            message: err.message,
-//            error: err
-//        });
-//    });
-//}
-//
-//production error handler
-//no stacktraces leaked to user
-//app.use(function (err, req, res, next) {
-//    res.status(err.status || 500);
-//    res.render('error', {
-//        message: err.message,
-//        error: {}
-//    });
-//});
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
 
 var testFlag = false;
 
